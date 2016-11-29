e219a28d56336fc70001b54606b0c04aa924ffaf
roxylu <roxylu816@gmail.com>
2016-11-29 13:33:16 +0800

    Add script for finding mouse

---
+++ find_mouse.py
@@ -0,0 +1,44 @@
+import sys 
+import tushare as ts
+import numpy as np
+
+from utils import get_all_stock_id
+
+#60000-604000
+#00000-003000
+
+def find_mouse(all_codes, date_start, date_end):
+    codes = []
+    for code in all_codes:
+        code = str(code).zfill(6)
+        try:
+            df=ts.get_k_data(code, start=date_start, end=date_end)
+            df['open'] = df['open'].astype(float)
+            df['close'] = df['close'].astype(float)
+            df['high'] = df['high'].astype(float)
+            df['low'] = df['low'].astype(float)
+            df['bottom'] = np.where(df['open']>=df['close'], df['close'], df['open']).astype(float)
+            df['percent'] = (df['bottom'] - df['low']) * 100 / df['bottom']
+            if df['percent'].max() > 5:
+                print "Found %s" % code
+                codes.append(code)
+        except:
+            print "Error %s" % code
+            pass
+    print "----------------------------"
+    return codes
+
+
+def write_to_file(filename, codes):
+    output_file = open(filename, 'w')
+    for code in codes:
+	output_file.write("%s\n" % code)
+    output_file.close()
+
+
+if __name__ == '__main__':
+    start = sys.argv[1]
+    end = sys.argv[2]
+    all_codes = get_all_stock_id()
+    codes = find_mouse(all_codes, start, end)
+    write_to_file('output.txt', codes)
---
+++ utils.py
@@ -0,0 +1,37 @@
+import datetime
+import os
+import sys 
+
+import pandas as pd
+import tushare as ts
+
+
+DOWNLOAD_DIR = os.path.pardir + '/stockdata/'
+STOCK_BASIC_CSV = DOWNLOAD_DIR + 'stock_basic_list.csv'
+
+#60000-604000
+#00000-003000
+
+def download_stock_basic_info():
+    df = ts.get_stock_basics()
+    df.to_csv(STOCK_BASIC_CSV)
+    print 'Download csv finished'
+
+
+def get_all_stock_id():
+    df = pd.DataFrame.from_csv(STOCK_BASIC_CSV)
+    ids = []
+    for i in df.index:
+        ids.append(i)
+    return ids
+
+"""
+def download_stock_kline(code, date_start='', date_end=datetime.date.today()):
+    code = str(code).zfill(6)
+
+    try:
+        file_name = 'h_kline_' + str(code) + '.csv'
+        write_mode = 'w'
+
+        if os.path.exists(
+"""
